e9a8367eaecbe169956f7d91ce9b6e32d17452ee
Joy Trinh <mongtuyen149@gmail.com>
2018-12-12 21:02:09 +0700

    Initial commit

---
+++ caesar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the key: ");
+            int key = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
